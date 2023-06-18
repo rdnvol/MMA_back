03cc79d274b930f880bbc7770c6ce227eb9ae0c1
rdnvol <rdnv.ol20@gmail.com>
2023-06-18 21:12:13 +0300

    added lesson types enum to swagger

--- src/lesson-types/entities/lesson-type.entity.ts
+++ src/lesson-types/entities/lesson-type.entity.ts
@@ -1,11 +1,12 @@
 import { LessonType } from '@prisma/client';
 import { ApiProperty } from '@nestjs/swagger';
 import { CoachEntity } from 'src/coaches/entities/coach.entity';
-import { CoachBusyLevel } from 'types';
+import { CoachBusyLevel, LessonType as LessonTypeEnum } from 'types';
 
 export class LessonTypeEntity implements LessonType {
   @ApiProperty() id: number;
-  @ApiProperty() type: string;
+  @ApiProperty({ enum: LessonTypeEnum, enumName: 'LessonTypeEnum' })
+  type: string;
   @ApiProperty({ type: CoachEntity, isArray: true }) coaches: CoachEntity[];
   @ApiProperty({ enum: CoachBusyLevel, enumName: 'CoachBusyLevel' })
   coachBusyLevel: CoachBusyLevel;
